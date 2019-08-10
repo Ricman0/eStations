aa07fe0af75c4dc5a33940a41f4335bd062dc77a
ricman89 <onizuka-89@hotmail.it>
2019-08-10 10:58:45 +0200

    Marker in l'aquila

--- app/src/main/java/it/univaq/estations/activity/MapsActivity.java
+++ app/src/main/java/it/univaq/estations/activity/MapsActivity.java
@@ -42,11 +42,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
+        LatLng laquila = new LatLng(42.367422, 13.349200);
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(laquila).title("Marker in L'Aquila"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(laquila));
     }
 }
