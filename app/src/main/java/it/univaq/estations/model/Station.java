295919e1a2e13c8633ba4d7a94a1f57bc3bdc582
ricman89 <onizuka-89@hotmail.it>
2019-09-25 12:32:40 +0200

    Added data request in StationList.java Added Station.java class

---
+++ app/src/main/java/it/univaq/estations/model/Station.java
@@ -0,0 +1,31 @@
+package it.univaq.estations.model;
+
+public class Station {
+
+    private long id;
+
+    private String name;
+
+    public Station(){}
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
