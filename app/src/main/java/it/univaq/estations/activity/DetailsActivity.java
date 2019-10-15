27bf575fa5b80655817efdfffaeb1b9b2f36e873
claudim <claudimarco@hotmail.it>
2019-10-15 11:36:52 +0200

    Added back arrow to come back to the station list activity

--- app/src/main/java/it/univaq/estations/activity/DetailsActivity.java
+++ app/src/main/java/it/univaq/estations/activity/DetailsActivity.java
@@ -44,7 +44,7 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mHandler = new Handler() {
 
