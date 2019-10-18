30687af8d941a8d250678865c215910681590a64
claudim <claudimarco@hotmail.it>
2019-10-18 17:56:36 +0200

    Merge branch 'master' of https://bitbucket.org/metf/estations
    # Conflicts:
    #	.idea/caches/gradle_models.ser
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/it/univaq/estations/activity/MapsActivity.java
+++ app/src/main/java/it/univaq/estations/activity/MapsActivity.java
@@ -1,6 +1,5 @@
 package it.univaq.estations.activity;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,12 +11,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import it.univaq.estations.R;
 import it.univaq.estations.model.Station;
